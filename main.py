a4cf92359c19fc60c453cf93794389346bb9560f
Patryk Kotarski <73948640+Patrykkotarski812@users.noreply.github.com>
2021-03-11 13:52:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+#ZAD1
+# lista = ['Ojciec Chrzestny', 'Django', 'Pulp Fiction', 'Fight Club', 'Władca pierścieni','Harry Potter', 'Wilk z Wall Street']
+# print(lista)
+# lista.reverse()
+# print(lista)
+# lista.insert(5,'Forest Gump')
+# lista.insert(5,'Czas Apokalipsy')
+# lista.insert(5,'Szeregowiec Ryan')
+# print(lista)
+
+# ZAD2
+# slownik = {'tytuł1': 'Ojciec Chrzestny', 'tytuł2': 'Django', 'tytuł3': 'Pulp Fiction', 'tytuł4': 'Fight Club', 'tytuł5': 'Władca pierścieni',
+#            'tytuł6':'Harry Potter', 'tytuł7': 'Wilk z Wall Street', 'tytuł8': 'Forest Gump', 'tytuł9': 'Czas Apokalipsy', 'tytuł10': 'Szeregowiec Ryan'}
+#
+# print(slownik)
+
+#ZAD3
+
+# slownik = {'CAD': 'Komputerowe wspomaganie programowania', 'PS': 'Programowanie strukturalne', 'JA': 'Język angielski',
+#            'WD': 'Wizualizacja danych', 'MD': 'Matematyka dyskretna',
+#            'AMDI':'Analiza matematyczna dla informatyków', 'WOT': 'Wiedza o teatrze'}
+# print(slownik)
+
+
+#ZAD4
+
+# a = input('Wprowadź liczbę a: ')
+# a = float(a)
+# liczba_do_potegi = a**a
+# print(liczba_do_potegi)
+
+
